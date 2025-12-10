f5c089f03280ea98e001ea3d40186f1fdad4b98f
lingyunzhang <lzhang772@wisc.edu>
2025-12-10 12:50:45 -0600

    font

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import "@fontsource/courgette";
+import "@fontsource/caveat";
+import "@fontsource/dancing-script";
+
 
 createRoot(document.getElementById('root')).render(
   
